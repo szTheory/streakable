6f766811287e95b5625be839a4ab55e08425bcbb
szTheory <szTheory@users.noreply.github.com>
2018-11-04 16:24:48 -0500

    more rdoc

--- lib/streakable.rb
+++ lib/streakable.rb
@@ -34,6 +34,16 @@ module Streakable
   module InstanceMethods # :nodoc:
     # Calculate a calendar streak. That is to say, the number of consecutive
     # days that exist for some date column, on an asociation with this object.
+    #
+    # For example if you have a User with many :posts, and one was created
+    # today that would be a streak of 1. If that user also created a Post yesterday,
+    # then the streak would be 2. If he created another Post the day before that one,
+    # he'd have a streak of 3, etc.
+    # 
+    # On the other hand imagine that the User hasn't created a Post yet today, but
+    # he did create one yesterday. Is that a streak? By default, it would be a streak of 0,
+    # so no. If you want to exclude the current day from this calculation, and count from
+    # yesterday, set +except_today+ to +true+.
     # 
     # @param [Symbol] association the ActiveRecord association on the instance
     # @param [Symbol] column the column on the association that you want to calculate the streak against.
@@ -43,7 +53,7 @@ module Streakable
       build_streak(association, column, except_today: except_today).length(longest: longest)
     end
 
-    # Calculate all calendar streaks. That is to say, the map of consecutive
+    # Calculate all calendar streaks. Returns a list of Date arrays. That is to say, a listing of consecutive
     # days counts that exist for some date column, on an asociation with this object.
     # 
     # @param [Symbol] association the ActiveRecord association on the instance
